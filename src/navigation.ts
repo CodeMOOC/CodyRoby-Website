83c2cd3a4f60cf150dbcea79eb0bccbcce24db5a
miris-mp <miriam.petrocchi94@gmail.com>
2024-11-22 17:57:09 +0100

    Update favicon and pages

--- src/navigation.ts
+++ src/navigation.ts
@@ -6,6 +6,8 @@ const metodiPath = '/metodi';
 const appPath = '/risorse';
 const otherMethods = '/altri-metodi'
 
+const currentYear = new Date().getFullYear();
+
 export const headerData = {
   links: [
     // {
@@ -146,14 +148,13 @@ export const footerData = {
     { text: 'Privacy Policy', href: getPermalink('/privacy') },
   ],
   socialLinks: [
-    { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
-    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
-    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
-    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
-    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
+    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: 'https://www.instagram.com/codemooc/' },
+    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: 'https://www.facebook.com/groups/CodeMOOC/' },
+    { ariaLabel: 'YouTube', icon: 'tabler:brand-youtube', href: 'https://www.youtube.com/channel/UCtcyXCuAy_NHusocxuERuXA' },
   ],
   footNote: `
-    <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
-    Made by <a class="text-blue-600 underline dark:text-muted" href="https://onwidget.com/"> onWidget</a> · All rights reserved.
+    
+    <p>&copy; ${currentYear} CodyRoby. Tutti i diritti riservati.</p>
+
   `,
 };
