85236ed46bb9bb4e32c079734d579dc85a0a613d
miris-mp <miriam.petrocchi94@gmail.com>
2024-11-25 20:42:02 +0100

    Optimize home and codymaze images

--- src/components/DynamicSpreadsheet.tsx
+++ src/components/DynamicSpreadsheet.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { parse } from 'csv-parse/browser/esm'; 
+import { parse } from 'csv-parse/browser/esm';
 import { Icon } from '@iconify/react';
 import IconSelect from './ui/IconSelect';
 
@@ -7,26 +7,23 @@ export default function DynamicSpreadsheet() {
   const [rows, setRows] = useState([]);
 
   const targets = ['infanzia', 'primaria', 'secondaria'];
-const metodi = ['CodyRoby', 'CodyColor', 'CodyFeet'];
-
+  const metodi = ['CodyRoby', 'CodyColor', 'CodyFeet'];
 
   useEffect(() => {
     async function fetchSpreadsheetData() {
       try {
-        const response = await fetch('https://docs.google.com/spreadsheets/d/e/2PACX-1vTcSleBVCqdjUx2ZI95NrW5jU4LIlSJp-F3YWD-BJ0t9utjvMzkila7eCXJMgUl9PMWQDrrzcK6i19r/pub?output=csv');
+        const response = await fetch(
+          'https://docs.google.com/spreadsheets/d/e/2PACX-1vTcSleBVCqdjUx2ZI95NrW5jU4LIlSJp-F3YWD-BJ0t9utjvMzkila7eCXJMgUl9PMWQDrrzcK6i19r/pub?output=csv'
+        );
         const csvData = await response.text();
 
-        parse(
-          csvData,
-          { columns: true, delimiter: ',' }, 
-          (err, records) => {
-            if (err) {
-              console.error('Error parsing CSV:', err);
-              return;
-            }
-            setRows(records); // Store the fetched and parsed data
+        parse(csvData, { columns: true, delimiter: ',' }, (err, records) => {
+          if (err) {
+            console.error('Error parsing CSV:', err);
+            return;
           }
-        );
+          setRows(records); // Store the fetched and parsed data
+        });
       } catch (error) {
         console.error('Error fetching spreadsheet data:', error);
       }
@@ -35,15 +32,14 @@ const metodi = ['CodyRoby', 'CodyColor', 'CodyFeet'];
     fetchSpreadsheetData();
   }, []);
 
-  
   return (
-    <div>
-        <div className='flex justify-center items-center gap-4'>
-        
-        <p >Filtri:</p>
-        <IconSelect metodi={metodi}/>
-        <IconSelect metodi={targets}/>
-       </div>
+    <div className="prose-page">
+      <div className="flex justify-start items-center gap-4 ">
+        <p className="font-semibold">Filtri:</p>
+        <IconSelect options={metodi} icon="codicon:symbol-method" filter="Metodi" />
+        <IconSelect options={targets} icon="pepicons-pop:label" filter="Toolkit" />
+      </div>
+      <hr className="my-6 not-prose" />
       {rows.length > 0 ? (
         <table>
           <thead>
@@ -56,7 +52,7 @@ const metodi = ['CodyRoby', 'CodyColor', 'CodyFeet'];
           <tbody>
             {rows.map((row, index) => (
               <tr key={index}>
-                {Object.values(row).map((value:string, colIndex) => (
+                {Object.values(row).map((value: string, colIndex) => (
                   <td key={colIndex}>{value}</td>
                 ))}
               </tr>
--- src/components/ui/IconSelect.tsx
+++ src/components/ui/IconSelect.tsx
@@ -1,46 +1,43 @@
 import React, { useEffect, useState } from 'react';
-import { parse } from 'csv-parse/browser/esm'; 
+import { parse } from 'csv-parse/browser/esm';
 import { Icon } from '@iconify/react';
 
-export default function IconSelect({metodi}) {
+export default function IconSelect({ options, icon, filter }) {
+  const toggleDropdown = () => setIsOpen(!isOpen);
 
-    const toggleDropdown = () => setIsOpen(!isOpen);
+  const [selected, setSelected] = useState('');
+  const [isOpen, setIsOpen] = useState(false);
 
-    const [selected, setSelected] = useState("");
-    const [isOpen, setIsOpen] = useState(false);
-  
-    const handleSelect = (metodo) => {
-      setSelected(metodo);
-    };
+  const handleSelect = (metodo) => {
+    setSelected(metodo);
+  };
 
-
-return(
-    
+  return (
     <div className="relative">
+      <button
+        className="bg-slate-400 text-white px-4 py-2 border-slate-400 capitalize flex items-center rounded-lg"
+        onClick={toggleDropdown}
+      >
+        <Icon icon={icon} className="inline-block mr-2" />
+        {filter}
+        <Icon icon="lsicon:down-filled" />
+      </button>
 
-<button
-className="bg-slate-400 text-white px-4 py-2 border-slate-400 capitalize flex items-center rounded-lg"
-onClick={toggleDropdown}
->
-<Icon icon="codicon:symbol-method" className="inline-block mr-2" />
-Metodi
-</button>
-
-{isOpen && (
-<ul className="absolute bg-white border rounded mt-2 shadow w-full max-w-[200px]">
-  {metodi.map((metodo) => (
-    <li
-      key={metodo}
-      className="px-4 py-2 capitalize cursor-pointer hover:bg-slate-200"
-      onClick={() => {
-        setIsOpen(false); // Close after selection
-      }}
-    >
-      {metodo}
-    </li>
-  ))}
-</ul>
-)}
-</div>
-
-)}
+      {isOpen && (
+        <ul className="absolute bg-white border rounded mt-2 shadow w-full max-w-[200px]">
+          {options.map((metodo) => (
+            <li
+              key={metodo}
+              className="px-4 py-2 capitalize cursor-pointer hover:bg-slate-200"
+              onClick={() => {
+                setIsOpen(false);
+              }}
+            >
+              {metodo}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
