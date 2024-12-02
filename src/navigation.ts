b5e679faa28c0fadeb5b08ff817ce82bb2ca8bf2
miris-mp <miriam.petrocchi94@gmail.com>
2024-12-02 11:34:17 +0100

    Created Prodotti e Pubblicazioni pages

--- src/navigation.ts
+++ src/navigation.ts
@@ -2,9 +2,10 @@ import { getPermalink, getAsset } from './utils/permalinks';
 
 // const basePath = import.meta.env.REPO_BASE;
 const basePath = '/';
-const metodiPath = '/metodi';
-const appPath = '/risorse';
-const otherMethods = '/altri-metodi'
+const metodiPath = `${basePath}/metodi`;
+const appPath = `${basePath}/risorse`;
+const otherMethods = '/altri-metodi';
+const pubAndProduct = `${basePath}/prodotti-e-pubblicazioni`;
 
 const currentYear = new Date().getFullYear();
 
@@ -19,26 +20,26 @@ export const headerData = {
       links: [
         {
           text: 'CodyRoby',
-          href: getPermalink(`${basePath}${metodiPath}/codyroby`),
+          href: getPermalink(`${metodiPath}/codyroby`),
         },
         {
           text: 'CodyColor',
-          href: getPermalink(`${basePath}${metodiPath}/codycolor`),
+          href: getPermalink(`${metodiPath}/codycolor`),
         },
         {
           text: 'CodyFeet',
-          href: getPermalink(`${basePath}${metodiPath}/codyfeet`),
+          href: getPermalink(`${metodiPath}/codyfeet`),
         },
         {
           text: 'Altri metodi',
           links: [
             {
               text: 'CodyWay',
-              href: getPermalink(`${basePath}${metodiPath}${otherMethods}/codyway`),
+              href: getPermalink(`${metodiPath}${otherMethods}/codyway`),
             },
             {
               text: 'Dress Code',
-              href: getPermalink(`${basePath}${metodiPath}${otherMethods}/dresscode`),
+              href: getPermalink(`${metodiPath}${otherMethods}/dresscode`),
             },
           ],
         },
@@ -49,7 +50,7 @@ export const headerData = {
       links: [
         {
           text: 'CodyMaze',
-          href: getPermalink(`${basePath}${appPath}/codymaze`),
+          href: getPermalink(`${appPath}/codymaze`),
         },
         {
           text: 'CodeHunting Games',
@@ -58,27 +59,27 @@ export const headerData = {
         },
         {
           text: 'CodyColor Game',
-          href: getPermalink(`${basePath}${appPath}/codycolor-game`),
+          href: getPermalink(`${appPath}/codycolor-game`),
         },
         {
           text: 'CodyTrip',
-          href: getPermalink(`${basePath}${appPath}/codytrip`),
+          href: getPermalink(`${appPath}/codytrip`),
         },
         {
           text: 'Coding in famiglia',
-          href: getPermalink(`${basePath}${appPath}/coding-in-famiglia`),
+          href: getPermalink(`${appPath}/coding-in-famiglia`),
         },
         {
           text: 'Progetti Scratch',
-          href: getPermalink(`${basePath}${appPath}/progetti-scratch`),
+          href: getPermalink(`${appPath}/progetti-scratch`),
         },
         {
           text: 'Videolezioni',
-          href: getPermalink(`${basePath}${appPath}/videolezioni`),
+          href: getPermalink(`${appPath}/videolezioni`),
         },
         {
           text: 'Attività in diretta',
-          href: getPermalink(`${basePath}${appPath}/attivita-in-diretta`),
+          href: getPermalink(`${appPath}/attivita-in-diretta`),
         },
       ],
     },
@@ -90,59 +91,16 @@ export const headerData = {
     },
     {
       text: 'Prodotti e Pubblicazioni',
-      href: getPermalink(`${basePath}/prodotti-e-pubblicazioni`),
+      links: [
+        { text: 'Prodotti', href: getPermalink(`${pubAndProduct}/prodotti`) },
+        { text: 'Pubblicazioni', href: getPermalink(`${pubAndProduct}/pubblicazioni`) },
+      ],
     },
   ],
   /*actions: [{text: 'Download', href: 'https://github.com/onwidget/astrowind', target: '_blank'}],*/
 };
 
 export const footerData = {
-  links: [
-    {
-      title: 'Product',
-      links: [
-        { text: 'Features', href: '#' },
-        { text: 'Security', href: '#' },
-        { text: 'Team', href: '#' },
-        { text: 'Enterprise', href: '#' },
-        { text: 'Customer stories', href: '#' },
-        { text: 'Pricing', href: '#' },
-        { text: 'Resources', href: '#' },
-      ],
-    },
-    {
-      title: 'Platform',
-      links: [
-        { text: 'Developer API', href: '#' },
-        { text: 'Partners', href: '#' },
-        { text: 'Atom', href: '#' },
-        { text: 'Electron', href: '#' },
-        { text: 'AstroWind Desktop', href: '#' },
-      ],
-    },
-    {
-      title: 'Support',
-      links: [
-        { text: 'Docs', href: '#' },
-        { text: 'Community Forum', href: '#' },
-        { text: 'Professional Services', href: '#' },
-        { text: 'Skills', href: '#' },
-        { text: 'Status', href: '#' },
-      ],
-    },
-    {
-      title: 'Company',
-      links: [
-        { text: 'About', href: '#' },
-        { text: 'Blog', href: '#' },
-        { text: 'Careers', href: '#' },
-        { text: 'Press', href: '#' },
-        { text: 'Inclusion', href: '#' },
-        { text: 'Social Impact', href: '#' },
-        { text: 'Shop', href: '#' },
-      ],
-    },
-  ],
   secondaryLinks: [
     { text: 'Terms', href: getPermalink('/terms') },
     { text: 'Privacy Policy', href: getPermalink('/privacy') },
@@ -150,7 +108,11 @@ export const footerData = {
   socialLinks: [
     { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: 'https://www.instagram.com/codemooc/' },
     { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: 'https://www.facebook.com/groups/CodeMOOC/' },
-    { ariaLabel: 'YouTube', icon: 'tabler:brand-youtube', href: 'https://www.youtube.com/channel/UCtcyXCuAy_NHusocxuERuXA' },
+    {
+      ariaLabel: 'YouTube',
+      icon: 'tabler:brand-youtube',
+      href: 'https://www.youtube.com/channel/UCtcyXCuAy_NHusocxuERuXA',
+    },
   ],
   footNote: `
     
