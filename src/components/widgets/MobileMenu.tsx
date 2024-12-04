548d03804baef04deaa3267531e9ada6c3fa2528
miris-mp <miriam.petrocchi94@gmail.com>
2024-12-04 09:38:30 +0100

    Fix mb menu

--- src/components/widgets/MobileMenu.tsx
+++ src/components/widgets/MobileMenu.tsx
@@ -93,6 +93,19 @@ const MobileMenu = ({ links, currentPath }) => {
                                   target={target2}
                                 >
                                   {text2}
+                                  {target2 && (
+                                    <svg
+                                      xmlns="http://www.w3.org/2000/svg"
+                                      width="1em"
+                                      height="1em"
+                                      viewBox="0 0 24 24"
+                                    >
+                                      <path
+                                        fill="currentColor"
+                                        d="M19 19H5V5h7V3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14c1.1 0 2-.9 2-2v-7h-2zM14 3v2h3.59l-9.83 9.83l1.41 1.41L19 6.41V10h2V3z"
+                                      />
+                                    </svg>
+                                  )}
                                 </a>
                               )}
                             </li>
@@ -111,6 +124,14 @@ const MobileMenu = ({ links, currentPath }) => {
                   target={target}
                 >
                   {text}
+                  {target && (
+                    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
+                      <path
+                        fill="currentColor"
+                        d="M19 19H5V5h7V3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14c1.1 0 2-.9 2-2v-7h-2zM14 3v2h3.59l-9.83 9.83l1.41 1.41L19 6.41V10h2V3z"
+                      />
+                    </svg>
+                  )}
                 </a>
               )}
             </li>
